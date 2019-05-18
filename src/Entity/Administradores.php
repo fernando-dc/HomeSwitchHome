59aab0637f352c961f3e1b1525aa5f946c5dece2
fernando-dc <fernando-dc@github.com>
2019-05-18 12:05:18 -0300

    Cambio en entidades
    Cambio en la estructura de algunas entidades por correcciones a la base de datos
    
    Co-Authored-By: victoria-acqua <victoria-acqua@users.noreply.github.com>
    Co-Authored-By: leopaulus <leopaulus@users.noreply.github.com>

--- src/Entity/Administradores.php
+++ src/Entity/Administradores.php
@@ -7,7 +7,7 @@ use Doctrine\ORM\Mapping as ORM;
 /**
  * Administradores
  *
- * @ORM\Table(name="administradores")
+ * @ORM\Table(name="administradores", indexes={@ORM\Index(name="email", columns={"email"})})
  * @ORM\Entity(repositoryClass="App\Repository\AdministradoresRepository")
  */
 class Administradores
@@ -21,13 +21,36 @@ class Administradores
      */
     private $token;
 
+    /**
+     * @var \Usuarios
+     *
+     * @ORM\ManyToOne(targetEntity="Usuarios")
+     * @ORM\JoinColumns({
+     *   @ORM\JoinColumn(name="email", referencedColumnName="email")
+     * })
+     */
+    private $email;
+
     public function getToken(): ?string
     {
         return $this->token;
     }
+
     public function setToken(int $token): self
     {
         $this->token = $token;
+
+        return $this;
+    }
+
+    public function getEmail(): ?Usuarios
+    {
+        return $this->email;
+    }
+
+    public function setEmail(?Usuarios $email): self
+    {
+        $this->email = $email;
 
         return $this;
     }
---
+++ src/Entity/Pujas.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Pujas
+ *
+ * @ORM\Table(name="pujas", indexes={@ORM\Index(name="email", columns={"email"}), @ORM\Index(name="id_subasta", columns={"id_subasta"})})
+ * @ORM\Entity(repositoryClass="App\Repository\PujasRepository")
+ */
+class Pujas
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id_puja", type="integer", nullable=false)
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="IDENTITY")
+     */
+    private $idPuja;
+
+    /**
+     * @var float
+     *
+     * @ORM\Column(name="monto", type="float", precision=10, scale=0, nullable=false)
+     */
+    private $monto;
+
+    /**
+     * @var \Usuarios
+     *
+     * @ORM\ManyToOne(targetEntity="Usuarios")
+     * @ORM\JoinColumns({
+     *   @ORM\JoinColumn(name="email", referencedColumnName="email")
+     * })
+     */
+    private $email;
+
+    /**
+     * @var \Subastas
+     *
+     * @ORM\ManyToOne(targetEntity="Subastas")
+     * @ORM\JoinColumns({
+     *   @ORM\JoinColumn(name="id_subasta", referencedColumnName="id_subasta")
+     * })
+     */
+    private $idSubasta;
+
+    public function getIdPuja(): ?int
+    {
+        return $this->idPuja;
+    }
+
+    public function getMonto(): ?float
+    {
+        return $this->monto;
+    }
+
+    public function setMonto(float $monto): self
+    {
+        $this->monto = $monto;
+
+        return $this;
+    }
+
+    public function getEmail(): ?Usuarios
+    {
+        return $this->email;
+    }
+
+    public function setEmail(?Usuarios $email): self
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    public function getIdSubasta(): ?Subastas
+    {
+        return $this->idSubasta;
+    }
+
+    public function setIdSubasta(?Subastas $idSubasta): self
+    {
+        $this->idSubasta = $idSubasta;
+
+        return $this;
+    }
+
+
+}
