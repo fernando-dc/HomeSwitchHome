9380f5ef7847e6b765cdcd88f009175282593002
fernando-dc <fernando-dc@github.com>
2019-06-15 20:18:55 -0300

    Agregadas fotos y cambios esteticos
    Actualizados los .css del filtro de residencias

--- src/Controller/FiltroController.php
+++ src/Controller/FiltroController.php
@@ -12,6 +12,8 @@ use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 
+use Symfony\Component\Validator\Constraints\GreaterThan;
+
 class FiltroController extends AbstractController
 {
     /**
@@ -22,15 +24,18 @@ class FiltroController extends AbstractController
         $defaultData = ['message' => 'Buscar residencias para el lugar:'];
         $form = $this->createFormBuilder($defaultData)
             ->add('lugar', TextType::class)
+
             ->add('fecha_inicial', DateType::class, [
                 'widget' => 'single_text',
                 'attr' => ['class' => 'js-datepicker-start', 'onkeydown'=>'return false', 'autocomplete'=>'off', ],
                 'html5' => false,
             ])
+
             ->add('fecha_final', DateType::class, [
                 'widget' => 'single_text',
                 'attr' => ['class' => 'js-datepicker-end', 'onkeydown'=>'return false', 'autocomplete'=>'off', ],
                 'html5' => false,
+
             ])
             ->add('buscar', SubmitType::class, ['label'=>'Buscar!', 'attr'=>['class'=> 'btn btn-success']])
             ->getForm();
@@ -42,6 +47,11 @@ class FiltroController extends AbstractController
 
             $formData = $form->getData();
             $em = $this->getDoctrine()->getManager();
+
+            $twoMonths = clone $formData['fecha_inicial'];
+            $twoMonths->add(date_interval_create_from_date_string('2 months'));
+            if ($formData['fecha_final'] <= $twoMonths) {
+            
 
             $residencias = $em->getRepository(Residencias::class)->residenciasEnCiudad($formData['lugar']);
             $semanasDisponibles =[];
@@ -61,8 +71,13 @@ class FiltroController extends AbstractController
                     $fecha_inicial ->add(date_interval_create_from_date_string('7 days'));
                 }
             }
-
             return $this->render('filtro/resultado.html.twig', ['semanas' => $semanasDisponibles]);
+
+            }
+            else {
+                $this->addFlash('danger','El rango de las fechas debe ser menor o igual a 2 meses!');
+            }
+
 
         }
         
